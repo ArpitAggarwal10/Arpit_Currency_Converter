aa6e7b316f526dd0813bead60abfe26874eaaff4
Arpit Aggarwal <arpitaggarwal1012@gmail.com>
2024-04-13 21:19:54 +0530

    added comments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 const freecurrencyapi = new Freecurrencyapi('fca_live_AuOBEpDIcL6gb5sKKJiXlmuXYHyBhWNFBT9Kc9rW');
 
+// convert fromCurrency to toCurrency units
 export async function convertCurrency(fromCurrency, toCurrency, units) {
     const res = await freecurrencyapi.latest({
         base_currency: fromCurrency,
